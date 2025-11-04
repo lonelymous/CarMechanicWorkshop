a1c0b82674e89597a8bbf9a6f8b5674fa9785de9
Szabó Ármin András <szabo.armin.andras@gmail.com>
2025-11-04 18:10:17 +0100

    Create JobCategoryEnum.cs file for Jobs

---
+++ CarMechanicWorkshop.Shared/Models/JobCategoryEnum.cs
@@ -0,0 +1,12 @@
+namespace CarMechanicWorkshop.Shared.Models;
+
+/// <summary>
+/// Enum representing different job categories in a car mechanic workshop.
+/// </summary>
+public enum JobCategoryEnum
+{
+    Chassis,
+    Engine,
+    Drivetrain,
+    Brakes,
+}
