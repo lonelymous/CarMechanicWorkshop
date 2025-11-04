6b425e2bb7913799af4a7e40fbf3f2d455351fb0
Szabó Ármin András <szabo.armin.andras@gmail.com>
2025-11-04 18:10:20 +0100

    Create JobStatusEnum.cs file for Jobs

---
+++ CarMechanicWorkshop.Shared/Models/JobStatusEnum.cs
@@ -0,0 +1,11 @@
+namespace CarMechanicWorkshop.Shared.Models;
+
+/// <summary>
+/// Represents the various status a job can be in.
+/// </summary>
+public enum JobStatusEnum
+{
+    Accepted,
+    InProgress,
+    Completed,
+}
