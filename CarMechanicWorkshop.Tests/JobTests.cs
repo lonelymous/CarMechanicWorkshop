b27020bd7cf852877d30ff2a3cdbcf979925097c
Szabó Ármin András <szabo.armin.andras@gmail.com>
2025-11-04 18:08:07 +0100

    Create Unit Tests csproj and a basic test

---
+++ CarMechanicWorkshop.Tests/JobTests.cs
@@ -0,0 +1,19 @@
+﻿using CarMechanicWorkshop.Shared.Models;
+using CarMechanicWorkshop.Shared.Models.Database;
+
+namespace CarMechanicWorkshop.Tests;
+
+public class JobTests
+{
+    [Fact]
+    public void JobtimeEstimation_ShouldBeCalculatedCorrectly()
+    {
+        var job = new JobDatabase
+        {
+            Category = JobCategoryEnum.Engine,
+            ManufacturingYear = DateTime.Now.Year - 10,
+            Severity = 5
+        };
+        Assert.Equal(8 * 1.5 * 0.6, job.EstimatedHours);
+    }
+}
