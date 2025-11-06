81295565939e4c94cc5d6bd2d75788d5891e862f
Szabó Ármin András <szabo.armin.andras@gmail.com>
2025-11-06 22:18:23 +0100

    Create Repository.cs

---
+++ CarMechanicWorkshop.API/Repositories/Repository.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using CarMechanicWorkshop.API.Interfaces;
+using CarMechanicWorkshop.API.Data;
+
+namespace CarMechanicWorkshop.API.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly CarMechanicWorkshopContext _context;
+        protected readonly DbSet<T> _dbSet;
+        public Repository(CarMechanicWorkshopContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+            => await _dbSet.AsNoTracking().ToListAsync();
+
+        public async Task<T?> GetByIdAsync(int id)
+            => await _dbSet.FindAsync(id);
+
+        public async Task CreateAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
