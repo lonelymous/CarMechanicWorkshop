f8f27bb66603da920d662353dc2804cef79148b2
Szabó Ármin András <szabo.armin.andras@gmail.com>
2025-11-07 00:15:44 +0100

    Update Program.cs

--- CarMechanicWorkshop.Client/Program.cs
+++ CarMechanicWorkshop.Client/Program.cs
@@ -7,7 +7,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "/api/") });
+var backendApiUrl = Environment.GetEnvironmentVariable("BackendApiUrl")
+                     ?? builder.HostEnvironment.BaseAddress; // fallback for local dev which is not used
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri($"{backendApiUrl.TrimEnd('/')}/api/")
+});
+
 
 builder.Services.AddScoped<ClientsApiService>();
 
