790c1566008c72a04d040c1490f086ffc863ed2d
Szabó Ármin András <szabo.armin.andras@gmail.com>
2025-11-06 23:58:58 +0100

    Comment ClientsService.cs

--- CarMechanicWorkshop.API/Services/ClientsService.cs
+++ CarMechanicWorkshop.API/Services/ClientsService.cs
@@ -5,6 +5,9 @@ using CarMechanicWorkshop.Shared.Models.DTOs;
 
 namespace CarMechanicWorkshop.API.Services;
 
+/// <summary>
+/// Service for managing ClientDatabase entities
+/// </summary>
 public class ClientsService : IClientsService
 {
     private readonly IRepository<ClientDatabase> _repository;
@@ -18,18 +21,32 @@ public class ClientsService : IClientsService
         _unitOfWork = unitOfWork;
     }
 
+    /// <summary>
+    /// Get all clients asynchronously
+    /// </summary>
+    /// <returns>A <see cref="IEnumerable{ClientDTO}"/> list of all clients</returns>
     public async Task<IEnumerable<ClientDTO>> GetAllAsync()
     {
         var clients = await _repository.GetAllAsync();
         return _mapper.Map<IEnumerable<ClientDTO>>(clients);
     }
 
+    /// <summary>
+    /// Get a client by its ID asynchronously
+    /// </summary>
+    /// <param name="id"> The ID of the client </param>
+    /// <returns> The <see cref="ClientDTO"/> client if found, otherwise null </returns>
     public async Task<ClientDTO?> GetByIdAsync(int id)
     {
         var client = await _repository.GetByIdAsync(id);
         return _mapper.Map<ClientDTO?>(client);
     }
 
+    /// <summary>
+    /// Create a new client asynchronously
+    /// </summary>
+    /// <param name="dto"> The <see cref="CreateClientDTO"/> client DTO to create </param>
+    /// <returns> The created <see cref="ClientDTO"/> </returns>
     public async Task<ClientDTO> CreateAsync(CreateClientDTO dto)
     {
         var entity = _mapper.Map<ClientDatabase>(dto);
@@ -48,6 +65,12 @@ public class ClientsService : IClientsService
         return _mapper.Map<ClientDTO>(entity);
     }
 
+    /// <summary>
+    /// Update an existing client asynchronously
+    /// </summary>
+    /// <param name="id"> The ID of the client to update </param>
+    /// <param name="dto"> The <see cref="UpdateClientDTO"/> client DTO to update </param>
+    /// <returns> The updated <see cref="ClientDTO"/> if found, otherwise null </returns>
     public async Task<ClientDTO?> UpdateAsync(int id, UpdateClientDTO dto)
     {
         var existing = await _repository.GetByIdAsync(id);
@@ -71,6 +94,11 @@ public class ClientsService : IClientsService
         return _mapper.Map<ClientDTO>(existing);
     }
 
+    /// <summary>
+    /// Delete a client by its ID asynchronously
+    /// </summary>
+    /// <param name="id"> The ID of the client to delete </param>
+    /// <returns> The success status of the operation </returns>
     public async Task<bool> DeleteAsync(int id)
     {
         var existing = await _repository.GetByIdAsync(id);
@@ -91,6 +119,11 @@ public class ClientsService : IClientsService
         }
     }
 
+    /// <summary>
+    /// Delete a client asynchronously
+    /// </summary>
+    /// <param name="dto"> The <see cref="ClientDTO"/> client DTO to delete </param>
+    /// <returns> The success status of the operation </returns>
     public async Task<bool> DeleteAsync(ClientDTO dto)
     {
         var entity = _mapper.Map<ClientDatabase>(dto);
