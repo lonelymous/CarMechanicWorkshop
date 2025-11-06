70a1444f4be11d6af9f92fc5d8c0c6ed9228c3b0
Szabó Ármin András <szabo.armin.andras@gmail.com>
2025-11-06 22:50:43 +0100

    Update ClientProfile.cs

--- CarMechanicWorkshop.API/Mapping/ClientProfile.cs
+++ CarMechanicWorkshop.API/Mapping/ClientProfile.cs
@@ -8,9 +8,18 @@ public class ClientProfile : Profile
 {
     public ClientProfile()
     {
+        // Entity ↔ DTO
         CreateMap<ClientDatabase, ClientDTO>().ReverseMap();
-        CreateMap<CreateClientDTO, ClientDatabase>();
+
+        // Create DTO → Entity
+        CreateMap<CreateClientDTO, ClientDatabase>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.Jobs, opt => opt.Ignore());
+
+        // Update DTO → Entity
         CreateMap<UpdateClientDTO, ClientDatabase>()
-            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.Jobs, opt => opt.Ignore())
+            .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
     }
 }
