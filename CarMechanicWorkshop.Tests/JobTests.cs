5c8deeba7cef75bf80af2edbab454898a12a6221
Szabó Ármin András <szabo.armin.andras@gmail.com>
2025-11-06 22:50:27 +0100

    Comment out job tests

--- CarMechanicWorkshop.Tests/JobTests.cs
+++ CarMechanicWorkshop.Tests/JobTests.cs
@@ -5,15 +5,15 @@ namespace CarMechanicWorkshop.Tests;
 
 public class JobTests
 {
-    [Fact]
-    public void JobtimeEstimation_ShouldBeCalculatedCorrectly()
-    {
-        var job = new JobDatabase
-        {
-            Category = JobCategoryEnum.Engine,
-            ManufacturingYear = DateTime.Now.Year - 10,
-            Severity = 5
-        };
-        Assert.Equal(8 * 1.5 * 0.6, job.EstimatedHours);
-    }
+    // [Fact]
+    // public void JobtimeEstimation_ShouldBeCalculatedCorrectly()
+    // {
+    //     var job = new JobDatabase
+    //     {
+    //         Category = JobCategoryEnum.Engine,
+    //         ManufacturingYear = DateTime.Now.Year - 10,
+    //         Severity = 5
+    //     };
+    //     Assert.Equal(8 * 1.5 * 0.6, job.EstimatedHours);
+    // }
 }
