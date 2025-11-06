7861b11a3085c017f05176042e3a641dc5994dd6
Szabó Ármin András <szabo.armin.andras@gmail.com>
2025-11-06 22:18:12 +0100

    Create ClientProfile.cs

---
+++ CarMechanicWorkshop.API/Mapping/ClientProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using CarMechanicWorkshop.API.Models.DTOs;
+using CarMechanicWorkshop.Shared.Models.Database;
+
+namespace CarMechanicWorkshop.API.Mapping;
+
+public class ClientProfile : Profile
+{
+    public ClientProfile()
+    {
+        CreateMap<ClientDatabase, ClientDTO>().ReverseMap();
+        CreateMap<CreateClientDTO, ClientDatabase>();
+        CreateMap<UpdateClientDTO, ClientDatabase>()
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+    }
+}
