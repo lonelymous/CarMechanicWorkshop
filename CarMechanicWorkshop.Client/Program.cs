a6474e9d9a22a712748cb5c389c8c1175449b948
Szabó Ármin András <szabo.armin.andras@gmail.com>
2025-11-10 20:49:39 +0100

    Update Program.cs

--- CarMechanicWorkshop.Client/Program.cs
+++ CarMechanicWorkshop.Client/Program.cs
@@ -7,17 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Read backend URL from environment or fallback to same origin
-var backendUrl = builder.Configuration["BackendApiUrl"] 
-                 ?? Environment.GetEnvironmentVariable("BACKEND_API") 
-                 ?? builder.HostEnvironment.BaseAddress + "api/";
-
-Console.WriteLine($"builder.Configuration[\"BackendApiUrl\"] | {builder.Configuration["BackendApiUrl"]}");
-Console.WriteLine($"Environment.GetEnvironmentVariable(\"BACKEND_API\") | {Environment.GetEnvironmentVariable("BACKEND_API")}");
-Console.WriteLine($"builder.HostEnvironment.BaseAddress | {builder.HostEnvironment.BaseAddress}");
-
 // Register HttpClient
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(backendUrl ?? "http://localhost:8080/api/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8080/api/") });
 
 // Register typed services
 builder.Services.AddScoped<ClientsApiService>();
